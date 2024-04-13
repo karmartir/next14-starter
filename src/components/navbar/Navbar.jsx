84a174e7fd146ec48de71a63c0fa3ec7f821f5c6
garen <karmartir@gmail.com>
2024-04-13 01:38:05 -0400

    making links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
+import Links from './Links'
 
 const Navbar = () => {
+
+
   return (
     <div>
-      Navbar
+     <div>Logo</div>
+     <div>
+       <Links/>
+     </div>
     </div>
   )
 }
