409583ed40691ef74fb79e98220b8974af1dd986
garen <karmartir@gmail.com>
2024-04-15 00:05:25 -0400

    home page design p1

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import styles from './about.module.css';
+
 const AboutPage = () => {
   return (
     <div>
-      AboutPage
+      <div className={styles.imgContainer}>
+     <Image src="/about.png" alt="about picture" fill />
+     </div>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,30 @@
-const Home = () => {
+/* eslint-disable react/no-unescaped-entities */
+import styles from "./home.module.css";
+import Image from "next/image";
 
-  return <div>Homepage</div>;
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Garen Martirosyan</h1>
+        <p>
+          A passionate Software Developer and creative UI/UX designer. I thrive
+          at the intersection of code and design, crafting seamless digital
+          experiences that captivate users.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn more</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src='/brands.png' alt='brands picture' fill className={styles.brandImg}/>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src='/hero.gif' alt='hero image' fill className={styles.heroImg}/>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
