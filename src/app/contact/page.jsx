fe335699d70cafec1e5978695e171a012cb7bc5e
garen <karmartir@gmail.com>
2024-04-15 03:07:46 -0400

    contact page p1

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,22 @@
+import styles from "./contact.module.css";
+import Image from "next/image";
 const ContactPage = () => {
   return (
-    <div>
-      ContactPage
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="/contact.png" alt='contact image' fill/>
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="First and Last Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="email" placeholder="Phone Number (optional)" />
+          <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea> 
+          <button>Send</button>
+        </form>
+      </div>
+
+
     </div>
   )
 }
