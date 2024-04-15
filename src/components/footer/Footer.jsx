bcfbe7d2c66bccc1400bb73a91097d331ac77e45
garen <karmartir@gmail.com>
2024-04-15 00:42:48 -0400

    home page p3

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className={styles.container}>
+    <div className={styles.logo}>"Kar<span style={{color: 'darkred'}}>M</span>a"</div>
+    <div className={styles.text}>
+   © {new Date().getFullYear()}. All rights reserved.
+    </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className={styles.container}>
-     <Link href='/' className={styles.logo}>Logo</Link>
+     <Link href='/' className={styles.logo}>"Kar<span style={{color: 'darkred'}}>M</span>a"</Link>
      <div>
        <Links/>
      </div>
