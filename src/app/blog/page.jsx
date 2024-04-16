349e62b050dbe70b98dd156d0b4ddd7737f134ee
garen <karmartir@gmail.com>
2024-04-15 22:20:56 -0400

    making blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,21 @@
+import styles from './blog.module.css'
+import PostCard from '@/components/postCard/postCard';
 const BlogPage = () => {
   return (
-    <div>
-      BlogPage
+    <div className={styles.container}>
+      <div  className={styles.post}>
+      <PostCard/>
+      </div>
+      <div  className={styles.post}>
+      <PostCard/>
+      </div>
+      <div  className={styles.post}>
+      <PostCard/>
+      </div>
+      <div  className={styles.post}>
+      <PostCard/>
+      </div>
+      
     </div>
   )
 }
