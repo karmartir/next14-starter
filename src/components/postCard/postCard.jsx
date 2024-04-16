4fe0f32389057950a0da7a1ab58edf56e33766b7
garen <karmartir@gmail.com>
2024-04-15 22:52:22 -0400

    blog continue

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -14,8 +14,9 @@ const PostCard = () => {
          </div>
        <div className={styles.bottom}>
        <h1 className={styles.title}>Title</h1>
-       <p className={styles.desc}>Desc</p>
-       <Link href='/blog/post'>Read more</Link> 
+       <p className={styles.desc}>
+I developed an internet store project, creating an intuitive and responsive user interface using HTML, CSS, JavaScript and React. Implemented features include product listings, shopping cart functionality, and secure checkout processes.</p>
+       <Link href='/blog/post'>Read more...</Link> 
        </div>
     </div>
 )}
