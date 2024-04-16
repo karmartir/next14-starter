1439b973e330893cf00148b13de33039fa835eaa
garen <karmartir@gmail.com>
2024-04-16 00:14:00 -0400

    blog continue

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,44 @@
+import styles from "./singlePost.module.css";
+import Image from "next/image";
+
 const SinglePostPage = () => {
-    return (
-        <div>
-            SinglePostPage
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="/project6.png" alt="" fill />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="/project6.png"
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry</span>
+          </div>
+
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
         </div>
-    );
+        <div className={styles.content}>
+          <p>
+            I built an internet store project with React, focusing on dynamic UI and
+            user experience. It features product display, interactive cart
+            management, and secure payment integration for a seamless shopping
+            journey. All design elements are meticulously crafted using custom
+            CSS to ensure a unique and engaging interface.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
