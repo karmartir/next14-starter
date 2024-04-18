de95636851118cd68d4f8d681c13e7b183b831b5
garen <karmartir@gmail.com>
2024-04-18 01:26:21 -0400

    dynamically fetch images

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -6,9 +6,9 @@ const PostCard = ({ post }) => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <div className={styles.imgContainer}>
-          <Image src="/project6.png" alt="" fill className={styles.img} />
-        </div>
+      {post.img &&  <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>}
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,25 +1,63 @@
 //TEMPORARY DATA
-const users = [
-  {id: 1, name: "John"},
-  { id: 2, name: "Jane" },
-];
+// const users = [
+//   {id: 1, name: "John"},
+//   { id: 2, name: "Jane" },
+// ];
 
-const posts = [
-  { id: 1, title: "Post 1", body: "......", userId: 1 },
-  { id: 2, title: "Post 2", body: "......", userId: 1 },
-  { id: 3, title: "Post 3", body: "......", userId: 2 },
-  { id: 4, title: "Post 4", body: "......", userId: 3 },
-];
+// const posts = [
+//   { id: 1, title: "Post 1", body: "......", userId: 1 },
+//   { id: 2, title: "Post 2", body: "......", userId: 1 },
+//   { id: 3, title: "Post 3", body: "......", userId: 2 },
+//   { id: 4, title: "Post 4", body: "......", userId: 3 },
+// ];
+import {Post, User} from '@/lib/models'
+import {connectToDb} from '@/lib/utils'
 
-export const getPosts = () => {
-    return posts
+
+export const getPosts = async () => {
+  try {
+    connectToDb();
+    const posts = await Post.find();
+    console.log(posts)
+    return posts;
+
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch posts!");
+  }
 };
 
-export const getPost = async (id) => {
-    const post = posts.find(post => post.id === parseInt(id));
-    console.log(post);
+export const getPost = async (slug) => {
+   try {
+    connectToDb();
+    const post = await Post.find(slug);
     return post;
-}
+    
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch posts!");
+  }
+};
 export const getUser = async (id) => {
-    return users.find(user => user.id === parseInt(id));
-}
+  try {
+    connectToDb();
+    const user = await User.findById(id);
+    return user;
+    
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch user!");
+  }
+};
+
+export const getUsers = async () => {
+  try {
+    connectToDb();
+    const users = await User.find();
+    return users;
+    
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch users!");
+  }
+};
