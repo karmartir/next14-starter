0a4b342b849d28e1a25263f05f25bc52d206aa8a
garen <karmartir@gmail.com>
2024-04-18 18:50:27 -0400

    deleting blogs in mongoDB from the server

--- src/app/serveractiontest/page.jsx
+++ src/app/serveractiontest/page.jsx
@@ -1,4 +1,5 @@
-import { addPost } from "@/lib/action";
+import { addPost, deletePost } from "@/lib/action";
+
 
 const ServerActionTestPage = () => {
 
@@ -15,6 +16,11 @@ const ServerActionTestPage = () => {
         <input type="text" placeholder="slug" name='slug' />
         <input type="text" placeholder="userId" name='userId' />
         <button>Create</button>
+      </form>
+
+      <form action={deletePost}>
+      <input type="text" placeholder="postId" name='id'/>
+      <button>Delete</button>
       </form>
     </div>
   );
