4fdff368b5b0d35ca1fcfff2c85f63d0bbd11254
garen <karmartir@gmail.com>
2024-04-20 00:52:47 -0400

    login css

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,15 +1,24 @@
+import { register } from "@/lib/action";
+import styles from "./register.module.css";
+
 const RegisterPage = () => {
-    return (
-        <div>
-        <form action="">
-            <input type="text" placeholder="username" name='username' />
-            <input type="email" placeholder="email" name='email' />
-            <input type="password" placeholder="password" name='password' />
-            <input type="password" placeholder="password again" name='passwordRepeat' />
-            <button>Register</button>
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form className={styles.form} action={register}>
+          <input type="text" placeholder="username" name="username" />
+          <input type="email" placeholder="email" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <input
+            type="password"
+            placeholder="password again"
+            name="passwordRepeat"
+          />
+          <button>Register</button>
         </form>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
