03fda8263bc94d00cb3091a3bef2ab44647ece8b
garen <karmartir@gmail.com>
2024-04-20 00:01:24 -0400

    login and making user in db

--- src/lib/models.js
+++ src/lib/models.js
@@ -1,59 +1,59 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required: true,
-        unique:true,
-        min:3,
-        max:20
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        max:50
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
     },
-    password:{
-        type:String,
-        required:true,
-        min:6
+    password: {
+      type: String,
     },
-    img:{
-        type:String,
+    img: {
+      type: String,
     },
-    isAdmin:{
-        type:Boolean,
-        default:false,
-    }
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timeseries: true }
+);
 
-
-}, {timeseries: true});
-
-const postSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required: true,
-       
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    desc:{
-        type:String,
-        required:true,
+    desc: {
+      type: String,
+      required: true,
     },
-    img:{
-        type:String,
+    img: {
+      type: String,
     },
-    userId:{
-        type:String,
-        required:true
+    userId: {
+      type: String,
+      required: true,
     },
-    slug:{
-        type:String,
-        required:true,
-        unique: true
-    }
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timeseries: true }
+);
 
-}, {timeseries: true});
-
-export const User = mongoose.models.User || mongoose.model("User", userSchema)
-export const Post = mongoose.models.Post || mongoose.model("Post", postSchema)
+export const User = mongoose.models?.User || mongoose.model("User", userSchema);
+export const Post = mongoose.models?.Post || mongoose.model("Post", postSchema);
