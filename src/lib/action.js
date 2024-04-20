ad9f5d7e722470583382bb489ee82168f0b117ad
garen <karmartir@gmail.com>
2024-04-20 00:40:40 -0400

    making registration page and logic

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,6 +1,6 @@
 "use server";
 import { connectToDb } from "./utils";
-import { Post } from "./models";
+import { Post, User } from "./models";
 import { revalidatePath } from "next/cache";
 import { signIn, signOut } from "./auth";
 
@@ -40,12 +40,39 @@ export const deletePost = async (formData) => {
   }
 };
 
-export const handleGithubLogin = async() => {
-  'use server'
-  await signIn('github')
-}
+export const handleGithubLogin = async () => {
+  "use server";
+  await signIn("github");
+};
 
-export const handleLogout = async() => {
-  'use server'
-  await signOut()
-}
+export const handleLogout = async () => {
+  "use server";
+  await signOut();
+};
+
+export const register = async (formData) => {
+  const { username, email, password, img, passwordRepeat } =
+    Object.fromEntries(formData);
+  if (password !== passwordRepeat) {
+    return "Password do not match";
+  }
+
+  try {
+    connectToDb();
+    const user = User.findOne({ username });
+    if (user) {
+      return "Username already exists";
+    }
+    const newUser = new User({
+      username,
+      email,
+      password,
+      img,
+    });
+    await newUser.save();
+    console.log("Saved to db");
+  } catch (error) {
+    console.log(error);
+    return { error: "Something went wrong!" };
+  }
+};
