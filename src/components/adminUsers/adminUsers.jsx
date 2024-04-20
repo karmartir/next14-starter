2497c678b49389d7e17554d601a6eb5f434ebc1c
garen <karmartir@gmail.com>
2024-04-20 19:16:48 -0400

    admin users logic

--- src/components/adminUsers/adminUsers.jsx
+++ src/components/adminUsers/adminUsers.jsx
@@ -1,9 +1,36 @@
+import { getUsers } from '@/lib/data';
 import styles from './adminUsers.module.css';
+import Image from 'next/image';
+import { deleteUser } from '@/lib/action';
 
-const AdminUsers = () => {
+const AdminUsers = async () => {
+
+ const users = await getUsers()
+
+
     return (
         <div className={styles.container}>
-            AdminUsers
+            <h1>Users</h1>
+           { users.map(user => (
+                <div className={styles.user} key={user.id}>
+                    <div className={styles.detail}>
+                    <Image
+                    src={user.img || '/noavatar.png'}
+                    alt=''
+                    width={50}
+                    height={50}
+                    />
+                    <span>{user.username}</span>
+                </div>
+
+             <form action={deleteUser}>
+                <input type="hidden" name='id' value={user.id} />
+                <button className={styles.userButton}>Delete</button>
+             </form>
+             </div>
+                ))}
+
+        
         </div>
     );
 };
